d0504c8bbc665031ba427fd5b5169cc01b7bfd0f
Viren Nadkarni <viren@outlook.com>
2016-06-14 17:16:26 +0530

    a better interrupt exception message

--- kernel/isr.c
+++ kernel/isr.c
@@ -37,41 +37,33 @@ void fault_handler(struct stack_frame *sf) {
         "Reserved",
     };
 
-    printf("----------------------------------------\n");
-    printf("Exception!");
     if( (sf->interrupt_no) < 32 )
-        printf(" %s", exception_message[ sf->interrupt_no ]);
-    printf("\nSystem Halted\n");
-    printf("----------------------------------------\n");
+        printf("[!] %s Exception\n", exception_message[ sf->interrupt_no ]);
+    else
+        printf("[!] Exception\n");
+    printf("[!] System Halted\n");
+    printf("                  error_code: %d\r", sf->error_code);
+    printf("> interrupt: %d\n", sf->interrupt_no);
 
-    printf("                error_code: %d\r", sf->error_code);
-    printf("interrupt: %d\n", sf->interrupt_no);
-    printf("\n");
+    printf("                  ebx: %#X\r", sf->ebx);
+    printf("> eax: %#X\n", sf->eax);
+    printf("                  edx: %#X\r", sf->edx);
+    printf("> ecx: %#X\n", sf->ecx);
 
-    printf("                ebx: %X\r", sf->ebx);
-    printf("eax: %X\n", sf->eax);
-    printf("                edx: %X\r", sf->edx);
-    printf("ecx: %X\n", sf->ecx);
-    printf("\n");
+    printf("                  edi: %#X\r", sf->edi);
+    printf("> esi: %#X\n", sf->esi);
+    printf("                  esp: %#X\r", sf->esp);
+    printf("> ebp: %#X\n", sf->ebp);
 
-    printf("                edi: %X\r", sf->edi);
-    printf("esi: %X\n", sf->esi);
-    printf("                esp: %X\r", sf->esp);
-    printf("ebp: %X\n", sf->ebp);
-    printf("\n");
+    printf("                  eflags: %#X\r", sf->eflags);
+    printf("> eip: %#X\n", sf->eip);
 
-    printf("                eflags: %X\r", sf->eflags);
-    printf("eip: %X\n", sf->eip);
-    printf("\n");
-
-    printf("                ds: %X\r", sf->ds);
-    printf("cs: %X\n", sf->cs);
-    printf("                fs: %X\r", sf->fs);
-    printf("es: %X\n", sf->es);
-    printf("                gs: %X\r", sf->gs);
-    printf("ss: %X\n", sf->ss);
-
-    printf("----------------------------------------\n");
+    printf("                  ds: %#X\r", sf->ds);
+    printf("> cs: %#X\n", sf->cs);
+    printf("                  fs: %#X\r", sf->fs);
+    printf("> es: %#X\n", sf->es);
+    printf("                  gs: %#X\r", sf->gs);
+    printf("> ss: %#X\n", sf->ss);
 
     while(1);
 }
