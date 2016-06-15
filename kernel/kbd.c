f33afc9dfcd31fc96ef50111198d7b1dc438b18e
Viren Nadkarni <viren@outlook.com>
2016-06-15 11:31:07 +0530

    keyboard enhancements
    - modifier keys for alphanumerics
    - keyboard led lights

--- kernel/kbd.c
+++ kernel/kbd.c
@@ -8,20 +8,19 @@
 #include "ctype.h"
 
 /* TODO:
- * - punctuation marks (!@#$%...)
+ * - differentiate between left & right modifers (right modifiers currently don't work)
  * - control characters (ctrl-c)
- * - caps/num/scroll lock
  * - function keys?
  * - modular keymaps instead of static definitions?
  */
 
 /* scancodes for US QWERTY */
-const uint8 US_QWERTY[128] = {
+const uint8 US_QWERTY_lower[128] = {
     0, 27 /* Esc */, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 8 /* Backspace */,
     9 /* Tab */, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 10 /* Enter */,
     0 /* Ctrl */, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`',
     0 /* Shift */, '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/',
-    0, 0 /* Alt */, ' ', 0 /* Caps lock */,
+    0, '*', 0 /* Alt */, ' ', 0 /* Caps lock */,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* Fn 1-10 */,
     0 /* Num lock */, 0 /* Scroll lock */, 0 /* Home */,
     0 /* Up arrow */, 0 /* Page up */, '-', 0 /* Left arrow */,
@@ -30,27 +29,48 @@ const uint8 US_QWERTY[128] = {
     0, 0 /* Fn 11-12 */, 0 /* other keys left undefined */,
 };
 
-#define SHIFT_HOLD      0x2A
-#define SHIFT_RELEASE   0xAA
+const uint8 US_QWERTY_upper[128] = {
+    0, 27 /* Esc */, '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 8 /* Backspace */,
+    9 /* Tab */, 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 10 /* Enter */,
+    0 /* Ctrl */, 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
+    0 /* Shift */, '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?',
+    0, '*', 0 /* Alt */, ' ', 0 /* Caps lock */,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0 /* Fn 1-10 */,
+    0 /* Num lock */, 0 /* Scroll lock */, 0 /* Home */,
+    0 /* Up arrow */, 0 /* Page up */, '-', 0 /* Left arrow */,
+    0, 0 /* Right arrow */, '+', 0 /* Down arrow */, 0 /* Page down */,
+    0 /* Insert */, 0 /* Delete */, 0, 0, 0,
+    0, 0 /* Fn 11-12 */, 0 /* other keys left undefined */,
+};
 
-#define ALT_HOLD        0x38
-#define ALT_RELEASE     0xb8
-#define RALT_MOD        0xe0
+#define SHIFT_HOLD          0x2A
+#define SHIFT_RELEASE       0xAA
 
-#define CTRL_HOLD       0x1d
-#define CTRL_RELEASE    0x9d
-#define RCTRL_MOD       0xe0
+#define ALT_HOLD            0x38
+#define ALT_RELEASE         0xB8
+#define RALT_MOD            0xE0
 
-static bool shift_pressed=false, alt_pressed=false, ctrl_pressed=false;
+#define CTRL_HOLD           0x1D
+#define CTRL_RELEASE        0x9D
+#define RCTRL_MOD           0xE0
+
+#define CAPS_LOCK           0x3A
+#define NUM_LOCK            0x45
+#define SCROLL_LOCK         0x46
+
+#define CAPS_LOCK_LIGHT     0x04
+#define NUM_LOCK_LIGHT      0x02
+#define SCROLL_LOCK_LIGHT   0x01
+
+static bool shift_pressed, alt_pressed, ctrl_pressed, caps_lock, num_lock, scroll_lock;
 
 void kbd_handler(struct stack_frame *sf) {
-    uint8 scancode, key;
+    uint8 scancode;
 
     scancode = inb(0x60);
 
     if(scancode & 0x80) {
         /* key has been released, indicated by MSB */
-        /* TODO: differentiate between Left and Right modifier keys */
         if(scancode == SHIFT_RELEASE)
             shift_pressed = false;
 
@@ -60,28 +80,71 @@ void kbd_handler(struct stack_frame *sf) {
         if(scancode == CTRL_RELEASE)
             ctrl_pressed = false;
 
-   } else {
+    } else {
         /* key has been pressed */
-        if(scancode == SHIFT_HOLD)
-            shift_pressed = true;
+        switch(scancode) {
+            case SHIFT_HOLD:
+                shift_pressed = true;
+                break;
+            case ALT_HOLD:
+                alt_pressed = true;
+                break;
+            case CTRL_HOLD:
+                ctrl_pressed = true;
+                break;
+            case CAPS_LOCK:
+                caps_lock = !caps_lock;
+                kbd_switchled(CAPS_LOCK_LIGHT);
+                break;
+            case NUM_LOCK:
+                num_lock = !num_lock;
+                kbd_switchled(NUM_LOCK_LIGHT);
+                break;
+            case SCROLL_LOCK:
+                scroll_lock = !scroll_lock;
+                kbd_switchled(SCROLL_LOCK_LIGHT);
+                break;
+        }
 
-        if(scancode == ALT_HOLD)
-            alt_pressed = true;
-
-        if(scancode == CTRL_HOLD)
-            ctrl_pressed = true;
-
-        key = US_QWERTY[scancode];
-
-        if(isalpha(key))
-            if(shift_pressed)
-                putchar(toupper(key));
-        else
-            putchar(key);
+        kbd_print(scancode);
     }
+}
+
+void kbd_print(uint8 scancode) {
+    uint8 c = shift_pressed ? US_QWERTY_upper[scancode] : US_QWERTY_lower[scancode];
+
+    if(caps_lock && isalpha(c)) {
+        if(isupper(c))
+            putchar(tolower(c));
+        else
+            putchar(toupper(c));
+    } else {
+        putchar(c);
+    }
+}
+
+void kbd_switchled(uint8 xor_status_byte) {
+    static uint8 led_status;
+    led_status ^= xor_status_byte;
+
+    /* wait if keyboard controller is busy */
+    while(true)
+        if( !(inb(0x64) & 0x2) )
+            break;
+
+    outb(0x60, 0xED);
+    outb(0x60, led_status);
 }
 
 void kbd_init() {
     irq_install_handler(1, kbd_handler);
+
+    shift_pressed = false;
+    alt_pressed = false;
+    ctrl_pressed = false;
+
+    caps_lock = false;
+    num_lock = false;
+    scroll_lock = false;
 }
 
--- kernel/kbd.h
+++ kernel/kbd.h
@@ -1,6 +1,10 @@
 #ifndef KBD_H_
 #define KBD_H_
 
+#include "def.h"
+
 void kbd_init(void);
+void kbd_print(uint8);
+void kbd_switchled(uint8);
 
 #endif
